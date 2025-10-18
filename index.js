3d96426a3a38210162248e5a01f25310c2fb5ae7
ibrahimmdef <ibrahimmusethis@gmail.com>
2025-10-18 12:43:02 +0400

    qr generated backend

--- index.js
+++ index.js
@@ -9,3 +9,34 @@ import qr from "qr-image";
 const app = express();
 
 const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const url = req.body.url;
+
+  let fileName = `qr_${Date.now()}.png`;
+
+  let qr_svg = qr.image(url);
+
+  const filePath = `public/images/${fileName}`;
+
+  qr_svg.pipe(fs.createWriteStream(filePath));
+
+  fs.writeFile("url.txt", url, (err) => {
+    if (err) throw err;
+    console.log("Url saved!");
+  });
+
+  res.render("index", { qrImage: fileName });
+});
+
+app.listen(port, () => console.log("Server running on http://localhost:3000"));
